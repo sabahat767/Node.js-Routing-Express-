c6ea5f336cd4a57c276a072a89457dcc21707bbf
sabahat767 <sabahatsabir12@gmail.com>
2021-01-05 12:55:26 +0500

    signUp/signIn/Routing Using Node.js

--- index.js
+++ index.js
@@ -19,11 +19,40 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static(Path.join(__dirname,'public')))
 //phle is public k folder ko static bnya coz yahn sare pages mojod hain
 //app.use(express.static(Path.join(__dirname,'registration')))
+let users=[
+    {name:"Faiza",id:1,email:'faz.pak@gmail.com',password:'111'},
+    {name:"Abeer",id:1,email:'abeer.pak@gmail.com',password:'123'}
+]
 app.get("/signup",(req,res)=>{
     res.sendFile(Path.join(__dirname,'registration','signup.html'))
 })
 app.post("/signup",(req,res)=>{
-    res.send(req.body)
+    //res.send(req.body)
+    let {email, name, password} = req.body //destructure object
+    let found = users.some((item)=>item.email == email)
+    if(found){
+        res.send('<h1>User already existed</h1>')
+    } else{
+        users.push({name,email,password,id:users.length+1})
+       // res.send('user added')
+      // res.sendFile(Path.join(__dirname, 'registration','signin.html'))
+      res.redirect('/signin')
+    }
+})
+app.get("/signin",(req,res)=>{
+    res.sendFile(Path.join(__dirname,'registration','signin.html'))
+})
+app.post("/signin",(req,res)=>{
+    let {email,password}=req.body
+    let found = users.some((item)=>item.email == email && item.password==password)
+    if(found){
+       // res.send('<h1>Welcome</h1>')
+    // res.sendFile(Path.join(__dirname, 'public','index.html'))
+res.redirect('/')
+     } else{
+      
+        res.sendFile(Path.join(__dirname, 'registration','signin.html'))
+    }
 })
 app.listen(Port,(req,res)=>{
     console.log('localHost:',Port)
